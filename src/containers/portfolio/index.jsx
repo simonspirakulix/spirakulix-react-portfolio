dd5c94e8029057398d1c40d03ace11cc4aa881f7
Simon Spirakulix <37393+simonspirakulix@users.noreply.github.com>
2023-04-10 21:31:16 +0100

    portfolio item 4 added

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
-import { AiFillProject } from "react-icons/ai";
+import { AiFillCloud, AiFillProject } from "react-icons/ai";
 import { filterOptions, portfolioData } from "./utils";
 import Iframe from 'react-iframe';
 import "./styles.scss";
@@ -26,7 +26,10 @@ const Portfolio = () => {
         headerText="Portfolio"
         icon={<AiFillProject size={40} />}
       />
-    <Iframe url="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FS7QQISHbUHGL5ANJjUuGVc%2FUXportfolio%25231%3Fpage-id%3D0%253A1%26node-id%3D1-2%26viewport%3D-3884%252C341%252C0.33%26scaling%3Dcontain%26starting-point-node-id%3D1%253A2" width="100%" height="1vh" id="uxlean_portfolio_iframe" display="block" position="relative" />
+    <div className="figma_portfolio_frame">
+    <Iframe url="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FS7QQISHbUHGL5ANJjUuGVc%2FUXportfolio%25231%3Fpage-id%3D0%253A1%26node-id%3D1-2%26viewport%3D-3884%252C341%252C0.33%26scaling%3Dcontain%26starting-point-node-id%3D1%253A2" width="100%" height="600px" id="uxlean_portfolio_iframe" display="block" position="relative" />
+    <h1> <AiFillCloud size={30} /> <a href="https://www.figma.com/proto/S7QQISHbUHGL5ANJjUuGVc/UXportfolio%231?page-id=0%3A1&node-id=3-17&viewport=407%2C315%2C0.33&scaling=contain&starting-point-node-id=1%3A2">View Portfolio on Figma.com</a></h1>
+    </div>
       <div className="portfolio__content">
         <ul className="portfolio__content__filter">
           {filterOptions.map((option) => (
