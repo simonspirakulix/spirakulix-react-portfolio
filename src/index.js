9eac029f1b50f6bd82b1c2d79f12d9bff96927d1
Simon Spirakulix <37393+simonspirakulix@users.noreply.github.com>
2023-04-11 18:08:33 +0100

    added GA4 Tag update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-
-
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+//Initialize GA4
+ReactGA.initialize("G-VN8ZRVBFHB");
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 const SendAnalytics = ()=> {
@@ -25,4 +25,8 @@ const SendAnalytics = ()=> {
     page: window.location.pathname,
   });
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(SendAnalytics);
